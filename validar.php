521eba8ac88f0b2fd9f9e45c323e457c41cb7951
Blvrr12 <bolillo3203@gmail.com>
2024-11-12 00:25:04 -0600

    Bolivar

--- validar.php
+++ validar.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+ 
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -59,7 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
 <?php
-
+/*
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
